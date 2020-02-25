54871cd9d22ac94af6a451ae30945a7f86ca52e2
Anant <anant.awasthy@tatadigital.com>
2020-02-25 16:47:45 +0530

    dn

--- public/bookmarlet.js
+++ public/bookmarlet.js
@@ -1,6 +1,11 @@
 (function(){
   console.log('Bookmarlet Starting')
   let _para = document.getElementsByTagName('p') 
+  let _dashboardsidebar = document.getElementsByClassName('dashboard-sidebar') 
+  if(_dashboardsidebar){
+    _dashboardsidebar.style['background-color'] = '#4A5568'  
+  }
+  
   for(let i = 0 ; i < _para.length ; ++i){
        _para[i].innerHTML = 'Hello World!'
       //_para[i].style['background-color'] = '#2C5282'
