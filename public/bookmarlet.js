e45a9ab4cec0ff54e27cf92934b9b1e799441649
Anant <anant.awasthy@tatadigital.com>
2020-02-25 16:59:38 +0530

    dn

--- public/bookmarlet.js
+++ public/bookmarlet.js
@@ -1,8 +1,9 @@
 (function(){
   console.log('Bookmarlet Starting')
   let _para = document.getElementsByTagName('p') 
-  let _dashboardsidebar = document.getElementsByClassName('dashboard-sidebar') 
-  if(_dashboardsidebar.length > 0){
+  
+  let _dashboardsidebar = document.getElementsByClassName('dashboard-sidebar js-sticky top-0 px-3 px-md-4 px-lg-4 overflow-auto')[0]
+  if(_dashboardsidebar){
     _dashboardsidebar.style['background-color'] = '#4A5568'  
   }
   
