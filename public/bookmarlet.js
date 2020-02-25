e744c774a52f45dada70594111206b0a79a3853e
Anant <anant.awasthy@tatadigital.com>
2020-02-25 16:43:45 +0530

    done

--- public/bookmarlet.js
+++ public/bookmarlet.js
@@ -2,8 +2,8 @@
   console.log('Bookmarlet Starting')
   let _para = document.getElementsByTagName('p') 
   for(let i = 0 ; i < _para.length ; ++i){
-      // _para[i].innerHTML = 'Anant'
-      _para[i].style['background-color'] = '#2C5282'
+       _para[i].innerHTML = 'Hello World!'
+      //_para[i].style['background-color'] = '#2C5282'
   }
 })()
 
